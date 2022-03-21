f8b09363812cbcf809eea139d268dd8c265c2b07
audrey sufrin <guyaudrey@orange.fr>
2022-03-21 15:20:05 -0400

    model créé

---
+++ db/migrate/20220321191901_create_relay_points.rb
@@ -0,0 +1,11 @@
+class CreateRelayPoints < ActiveRecord::Migration[6.1]
+  def change
+    create_table :relay_points do |t|
+      t.string :name
+      t.string :address
+      t.date :disponibility
+
+      t.timestamps
+    end
+  end
+end
