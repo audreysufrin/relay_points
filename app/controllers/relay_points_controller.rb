95a93fe40283042d2a909c8811a2169824d1fb92
rangom gilsa <rangom.gilsa@hotmail.com>
2022-03-24 11:48:36 -0400

    jai corrigé le controller

--- app/controllers/relay_points_controller.rb
+++ app/controllers/relay_points_controller.rb
@@ -1,34 +1,33 @@
 class RelayPointsController < ApplicationController
-  class RelayPointsController < ApplicationController
-    def index
-      @relay_points = RelayPoint.all
-    end
-    def show
-      @relay_point = RelayPoint.find(params[:id])
-    end
-    def new
-      @relay_point = RelayPoint.new
-    end
-    def create
-      @relay_point = RelayPoint.new(relay_point_params)
-      @relay_pointt.save
-      redirect_to relay_point_path(@relay_point)
-    end
-    def edit
-      @relay_point = RelayPoint.find(params[:id])
-    end
-    def update
-      relay_point = RelayPoint.find(params[:id])
-      relay_point.update(relay_point_params)
-      redirect_to relay_point_path(relay_point)
-    end
-    def destroy
-      relay_point = RelayPoint.find(params[:id])
-      relay_point.destroy
-      redirect_to relay_point_path
-    end
-    private
-    def relay_point_params
-      params.require(:relay_point).permit(:name, :name_shopify, :name_common, :hours, :localisation, :address, :contact, :status, :commune, :phone_number)
-    end
+  def index
+    @relay_points = RelayPoint.all
   end
+  def show
+    @relay_point = RelayPoint.find(params[:id])
+  end
+  def new
+   @relay_point = RelayPoint.new
+  end
+  def create
+    @relay_point = RelayPoint.new(relay_point_params)
+    @relay_pointt.save
+    redirect_to relay_point_path(@relay_point)
+    end
+  def edit
+    @relay_point = RelayPoint.find(params[:id])
+  end
+  def update
+    relay_point = RelayPoint.find(params[:id])
+    relay_point.update(relay_point_params)
+    redirect_to relay_point_path(relay_point)
+  end
+  def destroy
+    relay_point = RelayPoint.find(params[:id])
+    relay_point.destroy
+    redirect_to relay_point_path
+  end
+  private
+  def relay_point_params
+  params.require(:relay_point).permit(:name, :name_shopify, :name_common, :hours, :localisation, :address, :contact, :status, :commune, :phone_number)
+  end
+end
