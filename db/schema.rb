d0d7b31df49fcb51101b4a50859fdedaa91ad33e
rangom gilsa <rangom.gilsa@hotmail.com>
2022-03-28 16:26:54 -0400

    ajout dans la BDD day off

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_25_144044) do
+ActiveRecord::Schema.define(version: 2022_03_28_201929) do
 
   create_table "delivery_calendars", force: :cascade do |t|
     t.string "user"
