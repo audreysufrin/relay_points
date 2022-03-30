7810936c938740d080a58a9516d4571788f1cbc2
danielle misaine <daniellemisaine@gmail.com>
2022-03-30 08:50:18 -0400

    avancement dans controller

---
+++ app/controllers/day_offs_controller.rb
@@ -0,0 +1,33 @@
+class DayOffsController < ApplicationController
+  def index
+    @day_offs = DayOff.all
+  end
+
+  def new
+    @day_off = DayOff.new
+  end
+
+  def create
+    @day_off = DayOff.new(day_off_params)
+    @day_off.save!
+    redirect_to day_offs_path
+  end
+
+  def update
+    @day_off = DayOff.find(params[:id])
+    @day_off.update(day_off_params)
+    redirect_to day_off_path(@day_off)
+  end
+
+  def destroy
+    @day_off = DayOff.find(params[:id])
+    @day_off.destroy
+    redirect_to day_offs_path
+  end
+
+  private
+
+  def day_off_params
+    params.require(:day_off).permit(:date)
+  end
+end
