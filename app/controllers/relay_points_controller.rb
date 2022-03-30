54f008438040409204d3fbc44acd0ea553aa1177
audrey sufrin <guyaudrey@orange.fr>
2022-03-29 21:48:04 -0400

    modification

--- app/controllers/relay_points_controller.rb
+++ app/controllers/relay_points_controller.rb
@@ -33,9 +33,58 @@ class RelayPointsController < ApplicationController
     redirect_to relay_points_path
   end
 
+  def index_api
+    @relay_point = relay-points{
+      {
+        name: "Ta Nou",
+        address: "Baie des Tourelles - quartier Dillon",
+        hours: "7h à 12h et 13h à 15h",
+        status: "actif"
+      },
+      {
+        name: "Mme Maffre",
+        address: "23 lotissement Les Hameaux de la Pagerie",
+        hours: "15h à 19h",
+        status: "actif"
+      },
+      {
+        name: "Mme et Mr Murté",
+        address: "22 rue des Arawaks",
+        hours: "10h à 18h",
+        status: "actif"
+      },
+      {
+        name: "Dépôt de Bacchus",
+        address: "Quartier Bac, Pays Noyé",
+        hours: "10h à 12h et 14h à 18h",
+        status: "actif"
+      },
+      {
+        name: "Mr Marmot",
+        address: "Route des bénédictines",
+        hours: "14h à 17h",
+        status: "actif"
+      },
+      {
+        name: "Mr Pasua",
+        address: "2bis rue Gabriel Plateau fofo",
+        hours: "13h30 à 15h30 et 16h30 à 18h",
+        status: "actif"
+      },
+      {
+        name: "Mme et Mr Toris",
+        address: "Quartier Médecin",
+        hours: "10h à 12h et 13h à 17h",
+        status: "actif"
+      },
+    }
+
+  end
+
   private
 
   def relay_point_params
     params.require(:relay_point).permit(:name, :name_shopify, :name_common, :hours, :localisation, :address, :contact, :status, :commune, :phone_number)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :relay_points
   resources :delivery_calendars
+  get 'relay-points/api', to: 'relay-point#index-api'
 end
