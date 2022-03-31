7bb0c2e46dbda8c975360b835462e3ec362406c7
rangom gilsa <rangom.gilsa@hotmail.com>
2022-03-30 23:13:20 -0400

    admin en cours

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :relay_points
   resources :delivery_calendars
   resources :day_offs
+  root to: "relay_points#index"
   get "delivery_dates/next_delivery_date", to: "delivery_dates#next_delivery_date"
 end
