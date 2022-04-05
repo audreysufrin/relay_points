081c353ec34a6b0bd7d7f3b3c0545dd6e1b016a9
danielle misaine <daniellemisaine@gmail.com>
2022-04-05 10:46:37 -0400

    fond_blanc_sur_index_day_offs

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'relay_points/api', to: 'relay_points#index_api'
-  get 'next_delivery/api', to: 'delivery_dates#next_delivery_api'
   resources :relay_points
-  resources :day_offs
-
   root to: "relay_points#index"
+  get 'relay_points/api', to: 'relay_points#index_api'
+  resources :day_offs
+  get 'next_delivery/api', to: 'delivery_dates#next_delivery_api'
   get "next_delivery_date", to: "delivery_dates#next_delivery_date"
+
  end
