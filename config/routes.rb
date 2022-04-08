e24b8cc330467f12e0c23ee576495ec98dfa025a
danielle misaine <daniellemisaine@gmail.com>
2022-04-08 11:44:50 -0400

    routes à jour

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "relay_points#index"
   get 'relay_points/api', to: 'relay_points#index_api'
   resources :relay_points
-  root to: "relay_points#index"
+  get 'days_offs/api', to: 'days_offs#next_delivery_api'
   resources :day_offs
-  get 'next_delivery/api', to: 'delivery_dates#next_delivery_api'
-  get "next_delivery_date", to: "delivery_dates#next_delivery_date"
 end
